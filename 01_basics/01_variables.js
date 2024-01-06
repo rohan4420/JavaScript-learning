8644609bdc002a2f9f7a58885ef964a7a3686f80
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-01-06 07:03:43 +0000

    Varaibles in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accoutntId = 1234
+let accountEmail = "rohan@gmail.com"
+var accoutntName = "Rohan"
+accoutntCity = "Pune"
+let accountState;
+
+// accoutntId = 123
+accountEmail = "rahul@gmail.com"
+accoutntName = "Rahul"
+accoutntCity = "Bengluru"
+console.table([accoutntId,accountEmail,accoutntName,accoutntCity,accountState]);
+
+
+/*
+Perfer not to use var
+because of issue in block scope and functional scope
+*/
