51e3f554181a4b8a48e86fd0eb18582adfdcc5f3
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-01-06 07:02:31 +0000

    Data types

---
+++ 01_basics/02_Data_Types.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3+3 ); //we are using nodejs, not browser
+
+let name = "Rohan";
+let age = 24;
+let isLoggedIn = false;
+let state ;
+
+/* Primitive data types */
+// number =>2 to power 53
+// Bigint
+// string => " "
+// boolean => true/false
+// null => stand alone value(representation of empty value)
+// undefined => varaible is declared but value is not given type of (undefined)
+// symbol => used for uniqness(React)
+
+// object => type of (object)
+
+// console.log(typeof name); reserver keyword
+console.log(typeof age);
+console.log(typeof(age));
+
+console.log(typeof null);
