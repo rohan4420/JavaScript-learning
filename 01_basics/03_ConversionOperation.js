edabb03d0b90b3adbdee24d8c9430c70e0f33d50
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-01-06 07:03:00 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,31 @@
+let score = "44";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// nan => Nan
+// "33abc" => Nan
+// "" => 0
+// true => 1 false => 0
+
+let isLoggedIn = -0
+
+let booleanIsLogedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLogedIn);
+// console.log(booleanIsLogedIn);
+
+// "rohan" => true ; " " => false
+// 1 => true; 0 => false;(any number leaving 0 are true)
+
+let anyNumber = NaN
+let stringnumber = String(anyNumber)
+console.log(typeof stringnumber);
+console.log(stringnumber);
+
+// 33 => 33
+//true => true 
+//Nan => Nan
