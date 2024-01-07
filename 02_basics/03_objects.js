4aec4d0d036c9462bcb61244e5e2e8ee47f06773
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-01-07 08:51:14 +0000

    Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,48 @@
+// 2 ways to cretae obkect 
+// 1) literal
+// 2) constructor  //singleton
+
+// object literals
+
+// obkect.create //using contructer which is singleton
+//by defualt it takes key as string
+
+const mySym = Symbol("key1");
+
+const user = {
+    name : "rohan",
+    "fullName" : "Rohan Sardeshmukh",
+    [mySym]: "mykey1",
+    // mySym: "mykey1",
+    age : 18,
+    location : "Pune",
+    email : "rohan@gmal.com",
+    isLoggedIn : false,
+    isLastLoginDays : ["Monday","Saturday"]
+}
+
+// console.log(user.name);
+// console.log(user."fullName");
+// console.log(user["email"]);
+// console.log(user["fullName"]);  //perfer this to use
+
+// console.log(user[mySym]);
+// console.log(typeof mySym);
+
+// user.name = "rahul"
+// console.log(user);
+// Object.freeze(user)
+// user.name = "virat"
+// user.email = "rohan123@gmail.com"
+//it will not change as we have freezed the object
+// console.log(user);
+
+user.greeting = function(){
+    console.log("Hello Js user");
+}
+console.log(user.greeting()); //function is not executed but refreance is given to us
+
+user.greetingTwo = function(){
+    console.log(`Hello ! Everyone, how are you ${this.name}`);  //string interpollition
+}
+console.log(user.greetingTwo());
