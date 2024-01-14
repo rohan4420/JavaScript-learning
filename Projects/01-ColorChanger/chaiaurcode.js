0fddcc84464e41c29a28070723441024c7b28171
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-01-14 09:39:11 +0000

    This project for basic understanding of DOM.Also we got to know basics of eventListeners

---
+++ Projects/01-ColorChanger/chaiaurcode.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+// console.log(body);
+
+buttons.forEach( function (item){
+    // console.log(item);
+
+    //event
+    item.addEventListener('click',function(event){
+        // console.log(event);
+        // console.log(event.target);
+        if(event.target.id === 'grey'){
+            body.style.backgroundColor = event.target.id
+        }
+        else if(event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id
+        }
+        else if(event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id
+        }
+        else if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id
+        }
+    })
+})
