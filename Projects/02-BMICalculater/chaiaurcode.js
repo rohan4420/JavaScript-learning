400faa3697262dcbd6d2e986552da8b368f09950
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-01-14 10:52:28 +0000

    BMI calculater-Here we understood how to take text values and work on it

---
+++ Projects/02-BMICalculater/chaiaurcode.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form")
+// console.log(form);
+// this use case will give you empty value
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()  //dont submit on server
+    const height = parseInt(document.querySelector('#height').value)  //textbox gives value in string
+    const weight = parseInt(document.querySelector('#weight').value)
+    // console.log(height);
+    // console.log(width);
+    const results = document.querySelector('#results')
+
+
+
+    if(height === 0 || height < 0 || isNaN(height)){
+        results.innerHTML = `Please give valid height ${height}`
+    }
+    else if(weight === 0 || weight < 0 || isNaN(weight)){
+        results.innerHTML = `Please give valid weight ${weight}`
+    }
+    else{
+       const BMI = (weight / ((height*height)/10000)).toFixed(2)
+
+       //show the result
+       results.innerHTML = `<span>${BMI}</span>`
+    }
+    
+})
