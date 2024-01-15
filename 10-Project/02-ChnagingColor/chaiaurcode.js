085915771f2e1bbaca6b45da4a14b2617bf85685
rohan4420 <155799282+rohan4420@users.noreply.github.com>
2024-01-15 12:20:48 +0000

    Basic projects using set interval and addeventlisteners

---
+++ 10-Project/02-ChnagingColor/chaiaurcode.js
@@ -0,0 +1,31 @@
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let index = 0; index < 6;  index++) {
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+let setColor ;
+
+const startChangingColor = function(){
+    if(!setColor){
+        setColor = setInterval(changeBgColor,1000)
+    }
+    function changeBgColor(){
+    document.body.style.backgroundColor = randomColor()
+    }
+}
+
+const stopChangingColor = function(){
+
+    clearTimeout(setColor)
+    setColor = null
+}
+
+document.getElementById("start").
+            addEventListener('click',startChangingColor, false)
+
+document.getElementById("stop").
+            addEventListener('click',stopChangingColor, false)
